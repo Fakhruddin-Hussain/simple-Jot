4a2eb59a1834075f420f48ba40cc1242a3f2d3a8
Fakhruddin-Hussain <fakhruddinhussain65@gmail.com>
2025-05-30 01:14:45 +0530

    hashed password using bcrypt, salt and pepper

--- simplejot-backend/routes/auth.js
+++ simplejot-backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User=require('../models/Users');
 const { body,validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "@very$ecured$ecret";
+
 
 // create a new user using : POST "/api/auth/". doesn't require Auth
 // router.post('/',[
@@ -27,14 +32,31 @@ router.post('/createuser',[
     if(result.isEmpty()){
       const {name ,email ,password} = req.body;
       // searching for an existing email
-      let user = await User.findOne({email:email});
-      if(user){
+      let existinguser = await User.findOne({email:email});
+      if(existinguser){
         return res.status(400).json({error:"Sorry a user with this email already exists"});
       };
       try{
+        // secure the password using bcrypt hash adding salt to the encrypted password
+        // using async await because the bcrypt functions are returning a promise
+        const salt = await bcrypt.genSaltSync(10);
+        const secpassword= await bcrypt.hashSync(password,salt);
+
         // creating a new user using Users models
-        const user = await User.create({name,email,password});
-        return res.json(user);
+        const user = await User.create({name,email,password: secpassword});
+        // in this code I'm just getting the id of the user created in database to save as authtoken JWT
+        const data = {
+          user:{
+            id: user.id
+          }
+        }
+        // signing a JWT
+        const authtoken = jwt.sign(data, JWT_SECRET );
+        // console.log(jwtData);
+
+        // returning just the authoken as opposed to sending the full user data before
+        return res.json({authtoken});
+        // return res.json(user);
       }catch(err){
         console.log("Error: ",err)
         return res.status(500).json({error: "Server Error", message:err.message})
