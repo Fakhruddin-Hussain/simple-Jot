7c28068e18b7718810c5a2c89bcc184f39c0a1d7
Fakhruddin-Hussain <96165854+Fakhruddin-Hussain@users.noreply.github.com>
2025-05-30 10:54:29 +0530

    added dummy route

--- simplejot-backend/index.js
+++ simplejot-backend/index.js
@@ -8,6 +8,10 @@ const port = 5001
 app.use(express.json())
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
+app.get('/test-route', (req, res) => {
+  res.send("hello world")
+})
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
